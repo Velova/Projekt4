dabe6e6771165487693ca99993a127bfae459752
Your Name <you@example.com>
2021-04-30 14:56:12 +0200

    Fixed some css. Almost done with showing and unshowing cart items

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
 const dropdown = document.getElementById("dropdown");
 const dropdownmenu = document.getElementById("dropdownmenu");
+const cartdropdown = document.getElementById("cartdropdown");
+const cart = document.getElementById("cart");
 
 dropdown.addEventListener("click", DropDownVisible);
+cart.addEventListener("click", CartItemsShow);
 
 dropdownmenu.classList.add("hide");
+cartdropdown.classList.add("hide");
 
 
-let hide = true;
+let hidecategory = true;
 function DropDownVisible()
 {
-    if(hide)
+    if(hidecategory)
     {
         dropdownmenu.classList.remove("hide");
-        hide = false;
+        hidecategory = false;
     }
     else
     {
         dropdownmenu.classList.add("hide");
-        hide = true;
+        hidecategory = true;
     }
     console.log("DropDownVisible()");
 }
 
+
+
+let hidecart = true;
+function CartItemsShow()
+{
+    if(hidecart)
+    {
+        cartdropdown.classList.remove("hide");
+        hidecart = false;
+    }
+    else
+    {
+        cartdropdown.classList.add("hide");
+        hidecart = true;
+    }
+    console.log("Cartdropdown()");
+}
+
+
+
 Shoppingcart = window.localStorage;
+
