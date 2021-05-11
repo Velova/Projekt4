3697e48cc8527a0adb4a08d329f832570c1df0c6
Your Name <you@example.com>
2021-05-11 13:26:00 +0200

    Changed dropdown from js to css, added two html files, fixed some css

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+Shoppingcart = window.localStorage;
+
 const dropdown = document.getElementById("dropdown");
 const dropdownmenu = document.getElementById("dropdownmenu");
 const cartdropdown = document.getElementById("cartdropdown");
 const cart = document.getElementById("cart");
+const cartcontainer = document.getElementById("cartcontainer");
+
+const items = document.querySelectorAll(".addtocart");
+
+const item1name = localStorage.setItem("Name", "");
+const item1price = localStorage.setItem("Price", 0);
+const getitem1name = localStorage.getItem("Name");
+const getitem1price = localStorage.getItem("Price");
+
+console.log("Itemname: " + getitem1name);
+console.log("Itemprice: " + getitem1price);
+
+setTimeout
+let test = document.createElement("div");
+dropdownmenu.append(test);
+
+function TakeClickedId()
+{
+    let clickeditemid = parseInt(event.target.id);
+    if(clickeditemid == 1)
+    {
+        
+    }
+    else if(clickeditemid == 2)
+    {
+
+    }
+    console.log("TakeClickedId()");
+}
 
 dropdown.addEventListener("click", DropDownVisible);
 cart.addEventListener("click", CartItemsShow);
@@ -46,5 +77,5 @@ function CartItemsShow()
 
 
 
-Shoppingcart = window.localStorage;
+
 
